3d5a23593d6460f94322db3f3d86d237f44b98b7
b87775 <jwoods1@fullsail.edu>
2013-01-22 21:42:54 -0700

    Added object enteries(list of all 40 riders)

--- Woods_Jason_SDI1301_P3.js
+++ Woods_Jason_SDI1301_P3.js
@@ -4,13 +4,52 @@
 // Project 3
 // The Game
 
-var welcome = (" Welcome to your first experiance in Fantasy SuperCross!")
+var welcome = ("Welcome to your first experiance in Fantasy SuperCross!");
+
+
+
+
+
+
 var rules = { rule1: "1. First rule of SuperX fantasy is you must talk about it!",
 	rule2: "2. SuperX fantasy is a fanstasy style motor sport game",
 	rule3: "The more players the better the fun!"
 };
 
-var riders= { 
+
+
+var enteries = { sx: ["Ryan V. #1", "Ryan D. #5", "James S. #7", "Justin B. #10", "Kyle C. #11", "Jake W. #12", "David M. #18",
+ 	"Broc T. #20", "Chad R. #22", "Andrew S. #29", "Josh G. #33", "Rober K. #39", "Trey C. #41", "Vince F. #42", "Weston P. #46", "Matt L. #47",
+ 	"Justin B. #51", "Jimmy A. #55", "Matt G. #62", "Mike A. #800"],
+ 	sxLite: ["Eli T. #1", "Jason A. #21", "Jake C. #23", "Tyla R. #28", "Travis B. #31", "Malcolm S. #32", "Ryan S. #35", "Jessy N. #36", 
+ 	"Kyle C. #38","Martin D. #40", "Cole S. #43", "Christian C. #59", "Derek A. # 63", "Killian R. #66", "Dakota T. #72", "Max A. #74", 
+ 	"Austin P. #76", "Scott C. #78", "Michael L. #80", "Topher I. #91"]
+
+};
+
+
+
+
+
+
+console.log(welcome);
+console.log(enteries.sx)
+
+
+
+
+
+
+
+
+
+/*var welcome = (" Welcome to your first experiance in Fantasy SuperCross!")
+var rules = { rule1: "1. First rule of SuperX fantasy is you must talk about it!",
+	rule2: "2. SuperX fantasy is a fanstasy style motor sport game",
+	rule3: "The more players the better the fun!"
+};
+
+var riders= { sx: [" #1 Ryan V.","#5 Ryan D.","#7 James S.", "#10 Justin B.", "#11 Kyle C.", "#12 Jake W.",  ]
 
 };
 
@@ -37,6 +76,14 @@ for (var key in rules) {
 	console.log("key: " + key + ", value: ", rules[key]);
 
 };
+*/
+
+// IF NOT = if (!(a < b))
+// IF AND = if (a && c) 
+// IF OR = if (a || b)
+
+
+
 
 //Accessor Method:
 //aka "getter" going to be function
