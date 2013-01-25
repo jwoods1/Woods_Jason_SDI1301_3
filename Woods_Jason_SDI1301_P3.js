7719023a9922eaba01f7b02ccf261742ead54e28
b87775 <jwoods1@fullsail.edu>
2013-01-24 21:55:00 -0700

    Changed newArray fucntion to make it a method in json1

--- Woods_Jason_SDI1301_P3.js
+++ Woods_Jason_SDI1301_P3.js
@@ -13,9 +13,21 @@ var json1 = {
  	"sxLite": ["Eli T. #1", "Jason A. #21", "Jake C. #23", "Tyla R. #28", "Travis B. #31", "Malcolm S. #32", "Ryan S. #35", "Jessy N. #36", 
  		"Kyle C. #38","Martin D. #40", "Cole S. #43", "Christian C. #59", "Derek A. # 63", "Killian R. #66", "Dakota T. #72", "Max A. #74", 
  		"Austin P. #76", "Scott C. #78", "Michael L. #80", "Topher I. #91", "Ken R. #94", "Chris P. #98"]
- 	
+ 	},
+ 	"arrayShuffle": function (oldArray) {
+		var newArray = oldArray.slice();
+		var len = newArray.length;
+		var i = len;
+		while (i--) {
+			var p = parseInt(Math.random()*len);
+			var t = newArray[i];
+			newArray[i] = newArray[p];
+			newArray[p] = t;
+		}
+		return newArray;
 	}
 };
+
 //for loop
 var handleData = function (json) {
 	for (var i = 0; i < json.enteries.sx.length; i++){
@@ -42,63 +54,71 @@ var rules = { rule1: "1. First rule of SuperX fantasy is you must talk about it!
 
 
 //while loop /return array
-function arrayShuffle (oldArray) {
-	var newArray = oldArray.slice();
-	var len = newArray.length;
-	var i = len;
-	while (i--) {
-		var p = parseInt(Math.random()*len);
-		var t = newArray[i];
-		newArray[i] = newArray[p];
-		newArray[p] = t;
-	}
-	return newArray;
-};
 
 //method procedure
 var players = { 
 	jason: { name: "Jason", 
-		rank: " ", 
-		picks450: (arrayShuffle(json1.enteries.sx)), 
-		picks250: (arrayShuffle(json1.enteries.sxLite))
+		picks450: (json1.arrayShuffle(json1.enteries.sx)), 
+		picks250: (json1.arrayShuffle(json1.enteries.sxLite))
 	},
 	craig: { name: "Craig", 
-		rank: " ", 
-		picks450: (arrayShuffle(json1.enteries.sx)), 
-		picks250: (arrayShuffle(json1.enteries.sxLite))
+		picks450: (json1.arrayShuffle(json1.enteries.sx)), 
+		picks250: (json1.arrayShuffle(json1.enteries.sxLite))
 	},
 	jake: { name: "Jake", 
-		rank: " ", 
-		picks450: (arrayShuffle(json1.enteries.sx)), 
-		picks250: (arrayShuffle(json1.enteries.sxLite))
+		picks450: (json1.arrayShuffle(json1.enteries.sx)), 
+		picks250: (json1.arrayShuffle(json1.enteries.sxLite))
 	},
 	playerPicks: function (pick) {
 	for (var i = 0; i < pick.length; i++){
 		console.log(pick[i]);
 	};
+
+
 }
+
 };
+var winners250 = ("Winners for SX 250!!");
 var results = {
-	sx450: {sxResults: (arrayShuffle(json1.enteries.sx))} , 
-	sxLite: {sxLiteResults: (arrayShuffle(json1.enteries.sxLite)) } ,
-	playerRank: {},
-		raceResults: function (result) {
-			var log = console.log("<<<<<<<<<<<<THE RESULTS ARE IN!!!!!!!!!!!>>>>>>>>>>>>");
+	sx450: {sxResults: (json1.arrayShuffle(json1.enteries.sx))} , 
+	sxLite: {sxLiteResults: (json1.arrayShuffle(json1.enteries.sxLite)) } ,
+	raceResults: function (result,race450) {
+			console.log( "Results are what follows:");
 			for (var i = 0; i < result.length; i++){
-			console.log(result[i]);
+				console.log(result[i])
 			};
-			return log;
+			return console.log(race450);
+	},// boolean
+	playerRank: function (a, b) {
+   		var score = [];
+   		var total = [];
+   		if (a == b) { 
+   			score = 100
+   			
+  			console.log("Perfect Card!");
+
+  		} else{ 
+  			score = 5;
+  			points = a;
+
+  			console.log("You did not get a Perfect Card")
+  			};
+
+  			if (a !== b) {
+  				total = 5 + 10;
+
+  			} else{
+  				total = 10;
+  			};
+  			return total
 	}
-	
 };
 
-/*var playerPicks = function (pick) {
-	for (var i = 0; i < pick.length; i++){
-		console.log(pick[i]);
-	}
-}*/
 
-arrayShuffle(json1.enteries.sx);
+
+
+
+json1.arrayShuffle(json1.enteries.sx);
 console.log(welcome);
 console.log("<<<<<<<<<<<<<<<  >>>>>>>>>>>>>>>>>>>>");
 console.log(eRacersX);
@@ -107,6 +127,7 @@ console.log("<<<<<<<<<<<<<<<  >>>>>>>>>>>>>>>>>>>>");
 console.log(eRacersL);
 handleDataLite(json1);
 console.log("<<<<<<<<<<<<<<<  >>>>>>>>>>>>>>>>>>>>");
+
 console.log(players.jason.name + "s 450 picks are: ");
 players.playerPicks(players.jason.picks450);
 console.log("<<<<<<<<<<<<<<<  >>>>>>>>>>>>>>>>>>>>");
@@ -124,9 +145,22 @@ players.playerPicks(players.jake.picks450);
 console.log("<<<<<<<<<<<<<<<  >>>>>>>>>>>>>>>>>>>>");
 console.log(players.jake.name + "s 250 picks are: ");
 players.playerPicks(players.jake.picks250);
-console.log;
-results.raceResults(arrayShuffle(json1.enteries.sx));
-results.raceResults;
+console.log("<<<<<<<<<<<<<< >>>>>>>>>>>>>>>>>>>>");
+results.raceResults(json1.arrayShuffle(json1.enteries.sx),"The 450 Races are complete!");
+console.log("Jake 450 picks");
+results.playerRank(players.jake.picks450, results.raceResults);
+console.log("Jason 450 picks");
+results.playerRank(players.jason.picks450, results.raceResults);
+console.log("Craig 450 picks")
+results.playerRank(players.craig.picks450, results.raceResults);
+console.log("<<<<<<<<<<<<<< >>>>>>>>>>>>>>>>>>>>");
+console.log(winners250)
+results.raceResults(json1.arrayShuffle(json1.enteries.sxLite), "The 250 Races are complete!");
+console.log("Jake 250 picks");
+results.playerRank(players.jake.picks250, results.raceResults);
+console.log("Jason 250 picks");
+results.playerRank(players.jason.picks250, results.raceResults);
+console.log("Craig 250 picks")
 
 
 
@@ -134,114 +168,4 @@ results.raceResults;
 
 
 
-
-
-
-
-
-/*var welcome = (" Welcome to your first experiance in Fantasy SuperCross!")
-var rules = { rule1: "1. First rule of SuperX fantasy is you must talk about it!",
-	rule2: "2. SuperX fantasy is a fanstasy style motor sport game",
-	rule3: "The more players the better the fun!"
-};
-
-var riders= { sx: [" #1 Ryan V.","#5 Ryan D.","#7 James S.", "#10 Justin B.", "#11 Kyle C.", "#12 Jake W.",  ]
-
-};
-
-var player1 = { name: "Jake",
-	rank: []
-
-		
-};
-var player2 = { name: "Jason",
-	rank: []
-
-};
-
-var player3 = { name: "Craig",
-	rank:[]
-
-}
-
-console.log(rules);
-console.log(rules.rule2);
-
-console.log(welcome)
-for (var key in rules) {
-	console.log("key: " + key + ", value: ", rules[key]);
-
-};
-*/
-
-// IF NOT = if (!(a < b))
-// IF AND = if (a && c) 
-// IF OR = if (a || b)
-
-
-
-
-//Accessor Method:
-//aka "getter" going to be function
-
-//Mutator Method:  Procedure
-//change a property
-//aka " setter"
-
-//Prodedure:
-//Subroutine Funcion that doesn't return a value
-
-// dauntless, interceptor, bouty, Enterprise
-/*var dauntless = {name: "Dauntless", hold:[] };
-var interceptor = {name: "Inerceptor", hold:[] };
-var bounty = {name: "bounty", hold:[] };
-var enterprise = {name: "Enterprise", hold:[] };
-daunless.hold.push("oranges");
-*/
-
-/*var pirateShip = function (name) {
-	//......
-	return {
-		"name" : name,
-		"hold" : []
-	};
-};
-var dauntless = pirateShip("Dauntless");
-var interceptor = pirateShip("interceptor");
-var bounty = pirateShip("Bounty");
-var enterprise = pirateShip("Enterprise");
-
-
-
-
-console.log(dauntless);
-*/
-
-//property array
-/*var enteries = {	
-	"sx": ["Ryan V. #1", "Ryan D. #5", "James S. #7", "Justin B. #10", "Kyle C. #11", "Jake W. #12", "David M. #18",
- 		"Broc T. #20", "Chad R. #22", "Andrew S. #29", "Josh G. #33", "Rober K. #39", "Trey C. #41", "Vince F. #42", "Weston P. #46", "Matt L. #47",
- 		"Justin B. #51", "Jimmy A. #55", "Matt G. #62", "Mike A. #800", "Phillip N. #49", "Les S. #54"]
- 	},
- 	{
- 	"sxLite": ["Eli T. #1", "Jason A. #21", "Jake C. #23", "Tyla R. #28", "Travis B. #31", "Malcolm S. #32", "Ryan S. #35", "Jessy N. #36", 
- 		"Kyle C. #38","Martin D. #40", "Cole S. #43", "Christian C. #59", "Derek A. # 63", "Killian R. #66", "Dakota T. #72", "Max A. #74", 
- 		"Austin P. #76", "Scott C. #78", "Michael L. #80", "Topher I. #91", "Ken R. #94", "Chris P. #98"]
- 	}
- 		
-};*/
-//console.log(enteries.sx);
-//console.log(enteries);
-
-/*for (var i =0; i < enteries.sx.length; i++) {
-	console.log("450 SX Racer:");
-	console.log(enteries.sx[i]);
-};
-console.log(eRacersL);
-for (var i =0; i < enteries.sxLite.length; i++) {
-	console.log("250 Lite Racer:");
-	console.log(enteries.sxLite[i]);
-};
-
-console.log(players);*/
 
